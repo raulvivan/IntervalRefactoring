59108f952cc49da5f919c3c2066e29dc6a327283
raulvivan <raul.vivanco@opendeusto.es>
2015-03-27 17:19:11 +0100

    Muevo el metodo includes(double) a OpeningType, empiezo copiando el método y adaptandolo

--- src/intervals/OpeningType.java
+++ src/intervals/OpeningType.java
@@ -3,5 +3,20 @@ package intervals;
 public abstract class OpeningType {
 	
 	public abstract Opening getOpening();
+	
+	public boolean includes(double value, int minimum, int maximum) {
+		switch (this.getOpening()) {
+		case BOTH_OPENED:
+			return value > minimum && value < maximum;
+		case RIGHT_OPENED:
+			return value >= minimum && value < maximum;
+		case LEFT_OPENED:
+			return value > minimum && value <= maximum;
+		case UNOPENED:
+			return value >= minimum && value <= maximum;
+		default:
+			return false;
+		}
+	}
 
 }
