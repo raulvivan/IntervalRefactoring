2fcf63a5fcab7f7151bc9a41fd87e876065a2fcb
raulvivan <raul.vivanco@opendeusto.es>
2015-03-27 17:38:13 +0100

    Convertido metodo include(double) en clase padre en un metodo abstracto

--- src/intervals/OpeningType.java
+++ src/intervals/OpeningType.java
@@ -4,19 +4,6 @@ public abstract class OpeningType {
 	
 	public abstract Opening getOpening();
 	
-	public boolean includes(double value, double minimum, double maximum) {
-		switch (this.getOpening()) {
-		case BOTH_OPENED:
-			throw new RuntimeException ("Should be being overridden");
-		case RIGHT_OPENED:
-			throw new RuntimeException ("Should be being overridden");
-		case LEFT_OPENED:
-			throw new RuntimeException ("Should be being overridden");
-		case UNOPENED:
-			throw new RuntimeException ("Should be being overridden");
-		default:
-			return false;
-		}
-	}
+	public abstract boolean includes(double value, double minimum, double maximum);
 
 }
