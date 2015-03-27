c142ba1a321fa6fc32b1774c9696ada0ad31cfd7
raulvivan <raul.vivanco@opendeusto.es>
2015-03-27 12:29:39 +0100

    Añadidos en clase opening metodos que devuelven la subclase de OpeningType correspondiente a cada valor

--- src/intervals/Opening.java
+++ src/intervals/Opening.java
@@ -2,9 +2,30 @@ package intervals;
 
 public enum Opening {
 	
-	LEFT_OPENED, 
-	RIGHT_OPENED, 
-	BOTH_OPENED, 
-	UNOPENED;
+	LEFT_OPENED{
+		@Override
+		public OpeningType getType() {
+			return new OpeningLeft();
+		}
+	}, 
+	RIGHT_OPENED {
+		@Override
+		public OpeningType getType() {
+			return new OpeningRight();
+		}
+	}, 
+	BOTH_OPENED {
+		@Override
+		public OpeningType getType() {
+			return new OpeningOpened();
+		}
+	}, 
+	UNOPENED {
+		@Override
+		public OpeningType getType() {
+			return new OpeningUnopened();
+		}
+	};
 
+	public abstract OpeningType getType();
 }
