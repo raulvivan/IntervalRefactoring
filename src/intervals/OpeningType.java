663205f34112bbc689cb01c9f4191ba1c82f0ca4
raulvivan <raul.vivanco@opendeusto.es>
2015-03-29 18:43:00 +0200

    Copiado metodo a mover de intervalo a OpeningType

--- src/intervals/OpeningType.java
+++ src/intervals/OpeningType.java
@@ -6,4 +6,45 @@ public abstract class OpeningType {
 	
 	public abstract boolean includes(double value, double minimum, double maximum);
 
+	public boolean includes(Interval interval) {
+		if (this.openingType.getOpening().equals(
+				interval.openingType.getOpening())
+				|| this.getOpening().equals(Opening.UNOPENED)
+				|| interval.getOpening().equals(Opening.BOTH_OPENED)) {
+			return interval.minimum >= minimum && interval.maximum <= maximum;
+		} else {
+			if ((this.openingType.getOpening().equals(Opening.BOTH_OPENED) && interval.openingType
+					.getOpening().equals(Opening.UNOPENED))) {
+				return interval.minimum > minimum && interval.maximum < maximum;
+			} else if ((this.openingType.getOpening().equals(
+					Opening.BOTH_OPENED) && interval.getOpening().equals(
+					Opening.LEFT_OPENED))
+					|| (this.openingType.getOpening().equals(
+							Opening.RIGHT_OPENED) && interval.getOpening()
+							.equals(Opening.UNOPENED))
+					|| (this.openingType.getOpening().equals(
+							Opening.RIGHT_OPENED) && interval.openingType
+							.getOpening().equals(Opening.LEFT_OPENED))) {
+
+				return interval.minimum >= minimum
+						&& interval.maximum < maximum;
+
+			} else if ((this.openingType.getOpening().equals(
+					Opening.BOTH_OPENED) && interval.getOpening().equals(
+					Opening.RIGHT_OPENED))
+					|| (this.openingType.getOpening().equals(
+							Opening.LEFT_OPENED) && interval.getOpening()
+							.equals(Opening.UNOPENED))
+					|| (this.openingType.getOpening().equals(
+							Opening.LEFT_OPENED) && interval.openingType
+							.getOpening().equals(Opening.RIGHT_OPENED))) {
+
+				return interval.minimum > minimum
+						&& interval.maximum <= maximum;
+
+			} else
+				return false;
+		}
+
+	}
 }
