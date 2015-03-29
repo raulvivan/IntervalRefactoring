8e72ccc75a855403d6a2dd5599757ca27da1154f
raulvivan <raul.vivanco@opendeusto.es>
2015-03-29 19:38:01 +0200

    Convertido metodo includes(interval) de OpeningType en abstracto

--- src/intervals/OpeningType.java
+++ src/intervals/OpeningType.java
@@ -7,40 +7,6 @@ public abstract class OpeningType {
 	public abstract boolean includes(double value, double minimum,
 			double maximum);
 
-	public boolean includes(Interval interval, Interval mainInterval) {
-		if (this.getOpening().equals(interval.getOpening())
-				|| this.getOpening().equals(Opening.UNOPENED)
-				|| interval.getOpening().equals(Opening.BOTH_OPENED)) {
-			return interval.getMinimum() >= mainInterval.getMinimum()
-					&& interval.getMaximum() <= mainInterval.getMaximum();
-		} else {
-			if ((this.getOpening().equals(Opening.BOTH_OPENED) && interval
-					.getOpening().equals(Opening.UNOPENED))) {
-				return interval.getMinimum() > mainInterval.getMinimum()
-						&& interval.getMaximum() < mainInterval.getMaximum();
-			} else if ((this.getOpening().equals(Opening.BOTH_OPENED) && interval
-					.getOpening().equals(Opening.LEFT_OPENED))
-					|| (this.getOpening().equals(Opening.RIGHT_OPENED) && interval
-							.getOpening().equals(Opening.UNOPENED))
-					|| (this.getOpening().equals(Opening.RIGHT_OPENED) && interval
-							.getOpening().equals(Opening.LEFT_OPENED))) {
+	public abstract boolean includes(Interval interval, Interval mainInterval);
 
-				return interval.getMinimum() >= mainInterval.getMinimum()
-						&& interval.getMaximum() < mainInterval.getMaximum();
-
-			} else if ((this.getOpening().equals(Opening.BOTH_OPENED) && interval
-					.getOpening().equals(Opening.RIGHT_OPENED))
-					|| (this.getOpening().equals(Opening.LEFT_OPENED) && interval
-							.getOpening().equals(Opening.UNOPENED))
-					|| (this.getOpening().equals(Opening.LEFT_OPENED) && interval
-							.getOpening().equals(Opening.RIGHT_OPENED))) {
-
-				return interval.getMinimum() > mainInterval.getMinimum()
-						&& interval.getMaximum() <= mainInterval.getMaximum();
-
-			} else
-				return false;
-		}
-
-	}
 }
