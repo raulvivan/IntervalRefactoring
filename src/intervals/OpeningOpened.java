b26ae2564c208c93c37d058aac451335ec188619
raulvivan <raul.vivanco@opendeusto.es>
2015-03-29 18:56:16 +0200

    Creo includes en OpeningOpened que sobreescribe al de su clase padre

--- src/intervals/OpeningOpened.java
+++ src/intervals/OpeningOpened.java
@@ -11,6 +11,13 @@ public class OpeningOpened extends OpeningType{
 	public boolean includes(double value, double minimum, double maximum) {
 		return value > minimum && value < maximum;
 	}
+
+	@Override
+	public boolean includes(Interval interval, Interval mainInterval) {
+		return super.includes(interval, mainInterval);
+	}
+	
+	
 	
 	
 
