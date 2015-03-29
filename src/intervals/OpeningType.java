58ef13f4cf6059a7edffc1a75a1bea9f939469ad
raulvivan <raul.vivanco@opendeusto.es>
2015-03-29 19:33:06 +0200

    Implementado metodo includes(intrval) en OpeningLeft

--- src/intervals/OpeningType.java
+++ src/intervals/OpeningType.java
@@ -1,42 +1,38 @@
 package intervals;
 
 public abstract class OpeningType {
-	
+
 	public abstract Opening getOpening();
-	
-	public abstract boolean includes(double value, double minimum, double maximum);
+
+	public abstract boolean includes(double value, double minimum,
+			double maximum);
 
 	public boolean includes(Interval interval, Interval mainInterval) {
-		if (this.getOpening().equals(
-				interval.getOpening())
+		if (this.getOpening().equals(interval.getOpening())
 				|| this.getOpening().equals(Opening.UNOPENED)
 				|| interval.getOpening().equals(Opening.BOTH_OPENED)) {
-			return interval.getMinimum()>= mainInterval.getMinimum() && interval.getMaximum() <= mainInterval.getMaximum();
+			return interval.getMinimum() >= mainInterval.getMinimum()
+					&& interval.getMaximum() <= mainInterval.getMaximum();
 		} else {
 			if ((this.getOpening().equals(Opening.BOTH_OPENED) && interval
 					.getOpening().equals(Opening.UNOPENED))) {
-				return interval.getMinimum() > mainInterval.getMinimum() && interval.getMaximum() < mainInterval.getMaximum();
-			} else if ((this.getOpening().equals(
-					Opening.BOTH_OPENED) && interval.getOpening().equals(
-					Opening.LEFT_OPENED))
-					|| (this.getOpening().equals(
-							Opening.RIGHT_OPENED) && interval.getOpening()
-							.equals(Opening.UNOPENED))
-					|| (this.getOpening().equals(
-							Opening.RIGHT_OPENED) && interval
+				return interval.getMinimum() > mainInterval.getMinimum()
+						&& interval.getMaximum() < mainInterval.getMaximum();
+			} else if ((this.getOpening().equals(Opening.BOTH_OPENED) && interval
+					.getOpening().equals(Opening.LEFT_OPENED))
+					|| (this.getOpening().equals(Opening.RIGHT_OPENED) && interval
+							.getOpening().equals(Opening.UNOPENED))
+					|| (this.getOpening().equals(Opening.RIGHT_OPENED) && interval
 							.getOpening().equals(Opening.LEFT_OPENED))) {
 
 				return interval.getMinimum() >= mainInterval.getMinimum()
 						&& interval.getMaximum() < mainInterval.getMaximum();
 
-			} else if ((this.getOpening().equals(
-					Opening.BOTH_OPENED) && interval.getOpening().equals(
-					Opening.RIGHT_OPENED))
-					|| (this.getOpening().equals(
-							Opening.LEFT_OPENED) && interval.getOpening()
-							.equals(Opening.UNOPENED))
-					|| (this.getOpening().equals(
-							Opening.LEFT_OPENED) && interval
+			} else if ((this.getOpening().equals(Opening.BOTH_OPENED) && interval
+					.getOpening().equals(Opening.RIGHT_OPENED))
+					|| (this.getOpening().equals(Opening.LEFT_OPENED) && interval
+							.getOpening().equals(Opening.UNOPENED))
+					|| (this.getOpening().equals(Opening.LEFT_OPENED) && interval
 							.getOpening().equals(Opening.RIGHT_OPENED))) {
 
 				return interval.getMinimum() > mainInterval.getMinimum()
