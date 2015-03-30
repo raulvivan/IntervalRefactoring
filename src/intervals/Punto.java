014c907bbfa94221f47654d9848d4fdd582832ae
raulvivan <raul.vivanco@opendeusto.es>
2015-03-30 12:57:39 +0200

    Creada clase Punto con atributo constante de tipo double, constructor y getter del atributo

---
+++ src/intervals/Punto.java
@@ -0,0 +1,18 @@
+package intervals;
+
+public class Punto {
+	
+	private final double value;
+
+	public Punto(double value) {
+		super();
+		this.value = value;
+	}
+
+	public double getValue() {
+		return value;
+	}
+	
+	
+
+}
